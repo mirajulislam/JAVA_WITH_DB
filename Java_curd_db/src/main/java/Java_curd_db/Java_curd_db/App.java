97404980307d233b368b7c0ab33bfeb15789d55b
miraj <miraj@DESKTOP-VP27H1P>
2020-10-16 13:32:24 +0600

    logger configuration

--- Java_curd_db/src/main/java/Java_curd_db/Java_curd_db/App.java
+++ Java_curd_db/src/main/java/Java_curd_db/Java_curd_db/App.java
@@ -3,6 +3,9 @@ package Java_curd_db.Java_curd_db;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import model.Student;
 import service.StudentService;
 
@@ -12,13 +15,14 @@ import service.StudentService;
  */
 public class App 
 {
+	private static Logger logger =LogManager.getLogger(App.class);
     public static void main( String[] args )
     {
     	StudentService std=new StudentService();
 		List<Student> listStudent =std.getAllStudent();
-		for(Student leave : listStudent){
-		    System.out.println(leave.getAddress());
-		}
-		System.out.println(Arrays.toString(listStudent.toArray()));
+//		for(Student leave : listStudent){
+//		    System.out.println(leave.getAddress());
+//		}
+    	logger.error("student data"+Arrays.toString(listStudent.toArray()));
     }
 }
